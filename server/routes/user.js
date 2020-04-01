7cea0b146275d85bcaa2d1646cac6cbd4e6fd0ab
azputra <ahmadzp102@gmail.com>
2020-04-01 10:09:03 +0700

    replace login get to post

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const authenticate = require("../middleware/authentication")
 const authorize = require("../middleware/authorize")
 
 router.post('/register', userController.register)
-router.get('/login', userController.login)
+router.post('/login', userController.login)
 router.get('/send2FA/:userId', userController.send2FA)
 router.get('/check2FA/:userId', userController.check2FA)
 router.use(authenticate)
